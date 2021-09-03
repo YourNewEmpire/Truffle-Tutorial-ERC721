4d42b2e0efc4d37c29e1efd8acb9c21426c9a67d
Archie Smyth <archiesmyth26@googlemail.com>
2021-09-03 21:02:42 +0100

    comments added for methods totalsupply etc

--- contracts/GameItem.sol
+++ contracts/GameItem.sol
@@ -5,23 +5,27 @@ import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721UR
 import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
+
+
+
 contract GameItem is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-
     // set contract name and ticker. 
     constructor() ERC721("Contract Name", "TIC") {}
 
+    //get the current supply of tokens
     function totalSupply() public view returns (uint256) {
         return _tokenIds.current();
     }
 
-    // for opensea collection header
+    // for opensea collection 
     function contractURI() public pure returns (string memory) {
         return "https://ipfs.io/ipfs/your-collection-ipfshash";
     }
 
+    
     function mintItem(address player, string memory tokenURI)
         public
         onlyOwner
