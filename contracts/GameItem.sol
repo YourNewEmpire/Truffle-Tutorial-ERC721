f3cf67d55f330023a007b533c1d234566c964b5e
Archie Smyth <archiesmyth26@googlemail.com>
2021-09-03 19:53:38 +0100

    remove old, unused utils

--- contracts/GameItem.sol
+++ contracts/GameItem.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "../node_modules/@openzeppelin/contracts/utils/Strings.sol";
 
 contract GameItem is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
