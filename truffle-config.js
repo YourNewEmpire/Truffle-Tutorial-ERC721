b5ab6c2e7514ebb9250fab736691b17358b86755
Archie Smyth <archiesmyth26@googlemail.com>
2021-09-11 14:28:57 +0100

    Remove truffle-plugin-verify

--- truffle-config.js
+++ truffle-config.js
@@ -85,9 +85,7 @@ module.exports = {
   // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
   // those previously migrated contracts available in the .db directory, you will need to run the following:
   // $ truffle migrate --reset --compile-all
-  plugins: [
-    'truffle-plugin-verify'
-  ],
+ 
   db: {
     enabled: false
   }
